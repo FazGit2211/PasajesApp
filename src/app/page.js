d1add3a16281840a5babcb5e7164db128b010041
Facundo Zuleta <zuletafacundoadolfo2211@gmail.com>
2024-12-03 13:52:24 -0300

    agregracion estilos global

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import CardOferts from "./ui/cards/cardOferts";
 export default function Home() {
   return (
     <>
-    <div>
+    <div className="div-home">
       <h3>Ofertas de pasajes disponibles por tiempo limitado!</h3>
       <ul>
         <li><CardOferts/></li>
